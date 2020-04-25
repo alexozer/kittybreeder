f6e4528236dc7d015e5fd8f4f7ea03e3045be57d
Alex Ozer <alexozer@users.noreply.github.com>
2020-04-25 02:23:11 -0400

    Run arbitrary command with a "fakeshell" passed via fifo
    Still seems to have some strange race condition when launching terminals
    in quick succession, and my past attempts at this sort of thing have run
    into a similar issue...

--- kittybreeder.sh
+++ kittybreeder.sh
@@ -3,18 +3,16 @@ set -euo pipefail
 shopt -s nullglob
 IFS=$'\n\t'
 
-# TODO fix race condition when launching terminals in quick succession
-
 BG_TERMS=5
 RUNNING_DIR=/tmp/kittybreeder/running
-HIDDEN_DIR=/tmp/kittybreeder/hidden
+CLAIMED_DIR=/tmp/kittybreeder/claimed
 
 function fast_basename() {
     echo "${1##*/}"
 }
 
 function get_available_terms() {
-    for f in "$HIDDEN_DIR"/*; do
+    for f in "$RUNNING_DIR"/*; do
         echo "$f"
     done
 }
@@ -22,31 +20,43 @@ function get_available_terms() {
 function launch_terms() {
     curr_terms="$(get_available_terms | wc -l)"
     for ((i=curr_terms;i<BG_TERMS;i++)); do
-        id="$(uuidgen)"
-        kitty --name "$id" --listen-on unix:"$RUNNING_DIR"/"$id"& disown
-        ln -s "$RUNNING_DIR"/"$id" "$HIDDEN_DIR"/"$id"
+        term_uuid="$(uuidgen)"
+        running_term_path="$RUNNING_DIR"/"$term_uuid"
+        mkfifo "$running_term_path"
+        kitty --name "$term_uuid" "$0" fakeshell "$running_term_path" & disown
     done
 }
 
 function show_term() {
-    hidden_term_path="$(get_available_terms | head -n1)"
-    if [[ -z "$hidden_term_path" ]]; then
+    echo "one"
+    running_term_path="$(get_available_terms | head -n1)"
+    if [[ -z "$running_term_path" ]]; then
         echo 'Could not show a new terminal as none are currently launched'
         return 1
     fi
 
-    # Remove symlink ASAP
-    hidden_term_uuid="$(fast_basename "$hidden_term_path")"
-    active_term_path="$RUNNING_DIR"/"$hidden_term_uuid"
-    rm "$hidden_term_path"
+    echo "two"
+    term_uuid="$(fast_basename "$running_term_path")"
+    claimed_term_path="$CLAIMED_DIR"/"$term_uuid"
 
-    text=${2:-}
-    [[ -n "$text" ]] && kitty @ --to unix:"$active_term_path" send-text "$text"
+    echo "three"
+    # Attempt an atomic move to new dir to claim terminal, else try again
+    if ! mv "$running_term_path" "$claimed_term_path"; then
+        echo 'Race condition!'
+        return 1
+    fi
 
+    echo "four"
+    # Write command to run in terminal to FIFO
+    echo "$2" > "$claimed_term_path"
+    rm "$claimed_term_path"
+
+    echo "five"
+    # Show the terminal
     if [[ "$1" == "show" ]]; then
-        i3-msg "[instance=\"$hidden_term_uuid\"] scratchpad show, floating disable" > /dev/null
+        i3-msg "[instance=\"$term_uuid\"] scratchpad show, floating disable" > /dev/null
     elif [[ "$1" == "showfloat" ]]; then
-        i3-msg "[instance=\"$hidden_term_uuid\"] scratchpad show" > /dev/null
+        i3-msg "[instance=\"$term_uuid\"] scratchpad show" > /dev/null
     else
         echo 'Internal error: invalid command passed to show_term'
         return 1
@@ -54,7 +64,7 @@ function show_term() {
 }
 
 mkdir -p "$RUNNING_DIR"
-mkdir -p "$HIDDEN_DIR"
+mkdir -p "$CLAIMED_DIR"
 cmd="${1:-}"
 
 if [[ "$cmd" == "init" ]]; then
@@ -62,15 +72,15 @@ if [[ "$cmd" == "init" ]]; then
 
     # Remove sockets manually so we don't have to wait for terminals to die
     rm -r "$RUNNING_DIR"
-    rm -r "$HIDDEN_DIR"
+    rm -r "$CLAIMED_DIR"
     mkdir -p "$RUNNING_DIR"
-    mkdir -p "$HIDDEN_DIR"
+    mkdir -p "$CLAIMED_DIR"
 
     launch_terms
 
 elif [[ "$cmd" == "show" || "$cmd" == "showfloat" ]]; then
     set +e
-    show_term "$cmd" "${2:-}"
+    show_term "$cmd" "$2"
     show_term_ret=$?
     set -e
 
@@ -78,11 +88,11 @@ elif [[ "$cmd" == "show" || "$cmd" == "showfloat" ]]; then
 
     exit "$show_term_ret"
 
-# elif [[ "$cmd" == "fakeshell" ]]; then
-#     $(<$1)
+elif [[ "$cmd" == "fakeshell" ]]; then
+    eval $(<$2)
 
 else
     echo "Invalid command: $cmd"
-    echo "Usage: $(fast_basename $0) <init|show|showfloat>"
+    echo "Usage: $(fast_basename "$0") <init|show|showfloat>"
     exit 1
 fi
